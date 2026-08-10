398a7f227b7d2ee9047e77c170b27027176f63a8
Arthur <arthurcoelhom04@gmail.com>
2026-08-10 09:01:41 -0300

    refatoreção do spring security

---
+++ src/main/java/com/arthur/usuario/infrastructure/exceptions/dto/ErrorResponseDTO.java
@@ -0,0 +1,21 @@
+package com.arthur.usuario.infrastructure.exceptions.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ErrorResponseDTO {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+}
